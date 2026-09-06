b0d1845b0542451ebac839024e31fb0ff2914745
maksiomaksio464-glitch <maksiomaksio464@gmail.com>
2026-09-06 10:14:50 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,10 @@
 module.exports = {
-  // Czy proxy ma być włączone? (true = włączone, false = połącz bezpośrednio z Rendera)
   useProxy: true,
 
-  // Dane serwera proxy SOCKS5
-  proxyHost: '167.235.2.84', // np. '185.220.101.5'
-  proxyPort: 10801,                   // np. 1080 (zmienia się w zależności od proxy)
+  // Publiczny punkt dostępowy Anycast Cloudflare WARP
+  proxyHost: '162.159.192.1',
+  proxyPort: 2408,
 
-  // Jeśli proxy wymaga logowania, wpisz dane poniżej (jeśli nie, zostaw null)
   proxyUsername: null,
   proxyPassword: null
 };
